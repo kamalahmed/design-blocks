c80c97bbc3998b33ceab588476a760d991e09c86
Kamal <kamalacca@gmail.com>
2024-03-23 17:43:06 +0400

    add svg icon

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '9755adb1baa3aa839561');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '998aed10681fd7adaec1');
---
+++ src/blocks/curvy/icon.js
@@ -0,0 +1,7 @@
+const Icon = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" fill="#0073aa">
+		<path d="M0,64 C30,0 70,128 128,64 V128 H0 Z"></path>
+	</svg>
+);
+
+export default Icon;
