ea6fe148709298a793d8e04a72b843594b282f84
Kamal <kamalacca@gmail.com>
2024-03-27 06:30:11 +0400

    remove build folder and added to gitignore

--- build/blocks/curvy/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bd7773e28e9f33d71ab6');
--- build/blocks/curvy/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '8318441985a8ca0dc1cd');
